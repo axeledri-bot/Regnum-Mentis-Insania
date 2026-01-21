bd393e02bae1ca3652d653337c02029924c94727
DESKTOP-4UEP3RV\Dell <axeledri06@hotmail.com>
2026-01-20 20:22:11 -0600

    Version 0.2

---
+++ Assets/Scripts/InteractuarObjetos.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InteractuarObjetos : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Interactuar();
+        }
+    }
+
+    public void Interactuar()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Objeto.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Objeto : MonoBehaviour
+{
+   public void Interactuar()
+    {
+        Destroy(gameObject);
+    }
+}
