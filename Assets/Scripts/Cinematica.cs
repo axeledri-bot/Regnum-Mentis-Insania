5cd601fcfb4ed395f98f4c994832536e9f4d2c9b
Axel XP <axeledri06@hotmail.com>
2026-04-22 11:12:56 -0600

    cinemtica

--- Assets/Scripts/Cinematica.cs
+++ Assets/Scripts/Cinematica.cs
@@ -9,4 +9,16 @@ public class Cinematica : MonoBehaviour
     {
         texto.GetComponent<TextMeshProUGUI>().text = "Presiona E para Continuar";
     }
+    public void Sonido1()
+    {
+        AudioManager.instance.Play("Sonido1");
+    }    
+    public void Sonido2()
+    {
+        AudioManager.instance.Play("Sonido2");
+    } 
+    public void Sonido3()
+    {
+        AudioManager.instance.Play("Sonido3");
+    }
 }
